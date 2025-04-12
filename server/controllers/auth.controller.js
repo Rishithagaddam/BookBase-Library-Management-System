2f8eb7bb4c2108d7e03b5528d0dbeccfd6a71067
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2025-04-12 18:26:30 +0530

    Modifying login to take details from User model

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,46 +1,13 @@
 const bcrypt = require('bcryptjs');
 const Faculty = require('../models/faculty');
-
-exports.signup = async (req, res) => {
-    try {
-        const { facultyId, email, password } = req.body;
-
-        // Check if faculty exists with this email or facultyId
-        const existingFaculty = await Faculty.findOne({
-            $or: [
-                { facultyId },
-                { email }
-            ]
-        });
-        
-        if (existingFaculty) {
-            return res.status(400).json({ message: "Faculty already registered" });
-        }
-
-        // Create new faculty
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        const newFaculty = new Faculty({
-            facultyId,
-            email,
-            password: hashedPassword,
-            role: 'faculty' // Default role
-        });
-
-        await newFaculty.save();
-        res.status(201).json({ message: "Registration successful" });
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
-};
+const User = require('../models/user');
 
 exports.login = async (req, res) => {
     try {
         const { facultyId, password } = req.body;
 
         // Find faculty
-        const faculty = await Faculty.findOne({ facultyId });
+        const faculty = await User.findOne({ facultyId });
         if (!faculty) {
             return res.status(404).json({ message: "Faculty not found" });
         }
